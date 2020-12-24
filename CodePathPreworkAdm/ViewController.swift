64339ca464ff64459139baeea069595092a95b7d
alieualiou <88aliou@gmail.com>
2020-12-24 15:29:04 -0500

    finished basic functionalities (tip + total)

--- CodePathPreworkAdm/ViewController.swift
+++ CodePathPreworkAdm/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
     @IBAction func onTap(_ sender: Any) {
         print("Hello")
@@ -22,6 +26,19 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func calculateTip(_ sender: Any) {
+    
+    // Get the bill amount
+        let bill = Double(billField.text!)  ?? 0
+        
+    // Calculate the tip and total
+        let tip   = bill * 0.1
+        let total = bill + tip
+        
+    // Update the tip and total labels
+        tipLabel.text   = "$\(tip)"
+        totalLabel.text = "$\(total)"
+    }
     
 }
 
